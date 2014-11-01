6d35939d8ae993dd3fee818b286d1d11e83a391f
yishn <shen.yichuan@gmail.com>
2014-11-01 13:56:14 +0100

    Add Maze class

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,1 +1,6 @@
+from maze import *
+
 print('Hello World')
+
+m = Maze(5, 4);
+print(m.width)
---
+++ src/maze.py
@@ -0,0 +1,8 @@
+class Maze:
+    """A representation of a maze as a rectangular graph."""
+
+    def __init__(self, width, height):
+        """Initializes a new instance of the Maze class with given width and height."""
+
+        self.width = width
+        self.height = height
