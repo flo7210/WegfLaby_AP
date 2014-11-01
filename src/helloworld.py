b8ea607789eecd9e5617d0e5cfb6908f40a61813
yishn <shen.yichuan@gmail.com>
2014-11-01 18:21:41 +0100

    Add string representation of maze

--- src/helloworld.py
+++ src/helloworld.py
@@ -3,4 +3,13 @@ from maze import *
 print('Hello World')
 
 m = Maze(5, 4)
-print(m.width)
+m.add_edge((1, 2), (2, 2))
+m.add_edge((2, 2), (2, 3))
+m.add_edge((2, 3), (3, 3))
+m.add_edge((3, 3), (4, 3))
+m.add_edge((4, 3), (5, 3))
+m.add_edge((4, 3), (4, 2))
+m.add_edge((4, 2), (4, 1))
+m.add_edge((2, 4), (2, 3))
+
+print(m)
--- src/maze.py
+++ src/maze.py
@@ -6,12 +6,12 @@ class Maze:
 
         self.width = width
         self.height = height
-        self.reachable = dict()
+        self._reachable = dict()
 
         # Initialize self.reachable
-        for x in xrange(1, width):
-            for y in xrange(1, height):
-                self.reachable[(x, y)] = set()
+        for x in xrange(1, width + 1):
+            for y in xrange(1, height + 1):
+                self._reachable[(x, y)] = set()
 
     def has_vertex(self, x, y):
         """Returns whether the given vertex coordinates is on the graph or not."""
@@ -22,10 +22,46 @@ class Maze:
         """Returns the neighbors of the vertex given by x and y on the rectangle."""
 
         if not self.has_vertex(x, y): return []
-        neighbors = [(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)]
+        neighbors = [(x + 1, y), (x, y - 1), (x, y + 1), (x - 1, y)]
         return [v for v in neighbors if self.has_vertex(v[0], v[1])]
 
     def get_reachable_neighbors(self, x, y):
-        """Returns a list of reachable neighbors of the vertex given by x and y in the maze."""
+        """Returns a set of reachable neighbors of the vertex given by x and y in the maze."""
 
-        return [v for v in self.get_neighbors(x, y) if v in self.reachable[(x, y)]]
+        return self._reachable[(x, y)]
+    
+    def add_edge(self, v1, v2):
+        """Adds an edge between vertices v1 and v2 to indicate that they are reachable."""
+
+        if not self.has_vertex(v1[0], v1[1]) or not self.has_vertex(v2[0], v2[1]): return
+        if v1 not in self.get_neighbors(v2[0], v2[1]): return
+
+        self._reachable[v1].add(v2)
+        self._reachable[v2].add(v1)
+    
+    def __repr__(self):
+        result = ""
+
+        for y in range(1, self.height + 1):
+            for x in xrange(1, self.width + 1):
+                # Draw top line
+                if (x, y - 1) in self.get_reachable_neighbors(x, y):
+                    result += '+  '
+                else: result += '+--'
+
+            result += '+\n'
+
+            for x in xrange(1, self.width + 1):
+                # Draw vertical passage
+                if (x - 1, y) in self.get_reachable_neighbors(x, y):
+                    result += '   '
+                else: result += '|  '
+
+            result += '|\n'
+
+            if y == self.height:
+                for x in xrange(1, self.width + 1):
+                    # Draw bottom line
+                    result += '+--'
+
+        return result + '+'
