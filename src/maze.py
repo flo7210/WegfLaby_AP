f4e73b62d36ca412e1f5158b02901ba5740a0b3b
yishn <shen.yichuan@gmail.com>
2014-11-01 19:59:52 +0100

    Implement breadth-first search

--- src/maze.py
+++ src/maze.py
@@ -23,18 +23,21 @@ class Maze:
 
         if not self.has_vertex(x, y): return []
         neighbors = [(x + 1, y), (x, y - 1), (x, y + 1), (x - 1, y)]
-        return [v for v in neighbors if self.has_vertex(v[0], v[1])]
+        return [(x, y) for (x, y) in neighbors if self.has_vertex(x, y)]
 
     def get_reachable_neighbors(self, x, y):
-        """Returns a set of reachable neighbors of the vertex given by x and y in the maze."""
+        """Returns a set of reachable neighbors of the given vertex in the maze."""
 
         return self._reachable[(x, y)]
     
     def add_edge(self, v1, v2):
         """Adds an edge between vertices v1 and v2 to indicate that they are reachable."""
 
-        if not self.has_vertex(v1[0], v1[1]) or not self.has_vertex(v2[0], v2[1]): return
-        if v1 not in self.get_neighbors(v2[0], v2[1]): return
+        (x1, y1) = v1
+        (x2, y2) = v2
+
+        if not self.has_vertex(x1, y1) or not self.has_vertex(x2, y2): return
+        if v1 not in self.get_neighbors(x2, y2): return
 
         self._reachable[v1].add(v2)
         self._reachable[v2].add(v1)
@@ -44,6 +47,39 @@ class Maze:
 
         for i in range(1, len(path)):
             self.add_edge(path[i], path[i - 1])
+
+    def bfs(self, start, end):
+        """Performs a breadth-first search and returns an optimal path from start to end."""
+
+        queue = [start]
+        parent = dict()
+
+        # Initialize parent
+        for v in iter(self._reachable): parent[v] = None
+        parent[start] = start
+
+        while len(queue) > 0:
+            (x, y) = queue.pop(0)
+            if (x, y) == end: break
+
+            for v in self.get_reachable_neighbors(x, y):
+                if parent[v] is not None: 
+                    # Vertex v already visited
+                    continue
+                parent[v] = (x, y)
+                queue.append(v)
+
+        # Reconstruct path
+        path = [end]
+        vertex = end
+
+        while parent[vertex] != vertex:
+            if parent[vertex] is None: return []
+            path.append(parent[vertex])
+            vertex = parent[vertex]
+
+        path.reverse()
+        return path
     
     def __repr__(self):
         result = ""
