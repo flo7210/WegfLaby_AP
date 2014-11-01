9c1516dd735a949d717b3b5c4d68b83706e2d69e
yishn <shen.yichuan@gmail.com>
2014-11-01 17:13:21 +0100

    Implement basic graph properties

--- src/maze.py
+++ src/maze.py
@@ -6,3 +6,26 @@ class Maze:
 
         self.width = width
         self.height = height
+        self.reachable = dict()
+
+        # Initialize self.reachable
+        for x in xrange(1, width):
+            for y in xrange(1, height):
+                self.reachable[(x, y)] = set()
+
+    def has_vertex(self, x, y):
+        """Returns whether the given vertex coordinates is on the graph or not."""
+
+        return min(x, y) > 0 and x <= self.width and y <= self.height
+
+    def get_neighbors(self, x, y):
+        """Returns the neighbors of the vertex given by x and y on the rectangle."""
+
+        if not self.has_vertex(x, y): return []
+        neighbors = [(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)]
+        return [v for v in neighbors if self.has_vertex(v[0], v[1])]
+
+    def get_reachable_neighbors(self, x, y):
+        """Returns a list of reachable neighbors of the vertex given by x and y in the maze."""
+
+        return [v for v in self.get_neighbors(x, y) if v in self.reachable[(x, y)]]
