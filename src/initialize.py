068a4894880aff61f2c1eddeb6cc4eae83ca2318
yishn <shen.yichuan@gmail.com>
2015-02-19 15:47:58 +0100

    Convert tabs to spaces

--- src/initialize.py
+++ src/initialize.py
@@ -1,9 +1,8 @@
-from maze import Maze
 from balancer import Balancer
 from serial import Serial
 
 with Balancer(Serial(0)) as balancer:
-	for i in range(100):
-		balancer.add_command(balancer.width / 2, balancer.height / 2)
+    for i in range(100):
+        balancer.add_command(balancer.width / 2, balancer.height / 2)
 
-	balancer.start_listening()
+    balancer.start_listening()
