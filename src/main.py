a6bcaad09d16e32a8d71e88eb9fe10c5acbc5dfc
flo7210 <fnowak7210@gmail.com>
2015-03-04 17:25:29 +0100

    Änderungen an main.py.

--- src/main.py
+++ src/main.py
@@ -61,14 +61,14 @@ def run(path, maze):
         balancer.start_listening()
 
 def is_done(vertex, maze, dualmaze):
-    for neighbor in maze.get_neighbors(vertex[0], vertex[1]):
+    for neighbor in maze.get_neighbors(*vertex):
         if maze.has_edge(neighbor, vertex) == dualmaze.has_edge(neighbor, vertex):
             return False
 
     return True
 
 def detect_walls(anchor, maze, dualmaze):
-    neighbors_stack = [n for n in maze.get_neighbors(anchor[0], anchor[1]) if not is_done(n, maze, dualmaze)]
+    neighbors_stack = [n for n in maze.get_neighbors(*anchor) if not is_done(n, maze, dualmaze)]
 
     with Balancer(Serial(0)) as balancer:
         def balance_handler(destination, response, destination_reached):
@@ -83,29 +83,25 @@ def detect_walls(anchor, maze, dualmaze):
                     # Neighbor is reachable, add to maze
                     maze.add_edge(anchor, v_destination)
 
-                    # Avoid unnecessary return to the anchor
-                    if len(neighbors_stack) == 1 and neighbors_stack[0] == anchor:
-                        balance_handler.last_vertex = v_destination
-                        return
-
                 print maze.print_path([v_destination], neighbors_stack)
                 print
 
                 if len(neighbors_stack) == 0:
+                    balance_handler.last_vertex = v_destination
                     return
 
-                # Process neighbors stack
-                neighbor = neighbors_stack.pop()
-                (t_new, u_new) = to_touchscreen_coord(maze, balancer, neighbor)
-                balancer.add_command(t_new, u_new)
-
-                if neighbor != anchor:
+                if v_destination != anchor:
                     # Return to anchor before moving to next neighbor
-                    neighbors_stack.append(anchor)
+                    balancer.add_command(*to_touchscreen_coord(maze, balancer, anchor))
+                else:
+                    # Process neighbors stack
+                    neighbor = neighbors_stack.pop()
+                    balancer.add_command(*to_touchscreen_coord(maze, balancer, neighbor))
+
             else:
                 # We are not at the destination
                 if balance_handler.failcounter < 1 or v_destination == anchor:
-                    balancer.add_command(destination[0], destination[1])
+                    balancer.add_command(*destination)
 
                     if to_vertex(maze, balancer, (t, u)) == anchor:
                         balance_handler.failcounter += 1
@@ -142,27 +138,13 @@ def detect_maze(start, width, height):
         last_vertex = detect_walls(vertex, maze, dualmaze)
 
         # Fill stack; avoid unnecessary paths
-        stack.extend([v for v in maze.get_reachables(vertex[0], vertex[1]) if v != last_vertex and not is_done(v, maze, dualmaze)])
+        stack.extend([v for v in maze.get_reachables(*vertex) if v != last_vertex and not is_done(v, maze, dualmaze)])
         if vertex != last_vertex: stack.append(last_vertex)
 
     return maze
 
-# def detect_maze():
-#     m = Maze(7, 5)
-#     m.add_path([(1, 3), (1, 2), (1, 1), (2, 1), (1, 1), (1, 2), (2, 2), (2, 3),
-#                 (3, 3), (3, 2), (3, 1),
-#                 (4, 1), (5, 1), (6, 1), (7, 1), (7, 2), (5, 1), (5, 2), (6, 2),
-#                 (6, 3), (5, 3),
-#                 (4, 1), (4, 2), (4, 3), (4, 4), (3, 4), (4, 4), (4, 5), (5, 5),
-#                 (4, 5), (3, 5), (2, 5), (1, 5), (1, 4), (2, 4),
-#                 (4, 4), (5, 4), (6, 4), (6, 5), (7, 5), (7, 4), (7, 3)])
-#     return m
-
 if __name__ == "__main__":
-    e = Maze(7, 5)
-    with open('example.maze', 'r') as f:
-        s = f.read()
-        e.parse(s)
+    m = Maze(1, 1)
 
     answer = None
     while answer not in ['A', 'B']:
@@ -192,16 +174,27 @@ if __name__ == "__main__":
             m = detect_maze(start, width, height)
             print('Maze detected!')
 
-        name = raw_input('Filename (w/o extension): ')
+        name = raw_input('File name (w/o extension): ')
         with open(name + '.maze', 'w') as f:
             f.write(repr(m))
+        print ('String representation saved.')
 
-    if answer == 'B':
+    elif answer == 'B':
         name = raw_input('File name (w/o extension): ')
-        n = Maze(1, 1)
         with open(name + '.maze', 'r') as f:
             s = f.read()
-            n.parse(s)
+            m.parse(s)
 
-    #path = e.bfs((1, 3), (7, 3))
-    #run(path, e)
+        start = raw_input('Starting point (input format is "x,y"): ')
+        finish = raw_input('Finish (input format is "x,y"): ')
+
+        s = start.partition(',')
+        f = finish.partition(',')
+
+        a = (int(s[0]), int(s[2]))
+        b = (int(f[0]), int(f[2]))
+
+        print('Please place the ball near ' + repr(a))
+
+        path = m.bfs(a, b)
+        run(path, m)
