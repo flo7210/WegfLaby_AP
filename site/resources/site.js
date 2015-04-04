fad17d13c5e321c6f681ebb3a75f5c1ce103127e
yishn <shen.yichuan@gmail.com>
2015-04-04 21:27:56 +0200

    Add TOC

--- site/resources/site.js
+++ site/resources/site.js
@@ -1,2 +1,34 @@
+function createLink(href, innerHTML) {
+    var a = document.createElement('a');
+    a.setAttribute('href', href);
+    a.innerHTML = innerHTML;
+    return a;
+}
+
+function generateTOC(toc) {
+    var i2 = 0, i3 = 0, needToc = false;
+    var elements = document.getElementsByTagName('article')[0];
+    toc = toc.appendChild(document.createElement('ol'));
+
+    for (var i = 0; i < elements.childNodes.length; ++i) {
+        var node = elements.childNodes[i];
+        var tagName = node.nodeName.toLowerCase();
+
+        if (tagName == 'h3') {
+            needToc = true;
+            if (++i3 == 1) toc.lastChild.appendChild(document.createElement('ol'));
+            toc.lastChild.lastChild.appendChild(document.createElement('li')).appendChild(createLink('#' + node.id, node.innerHTML));
+        } else if (tagName == 'h2') {
+            ++i2, i3 = 0;
+            toc.appendChild(document.createElement('li')).appendChild(createLink('#' + node.id, node.innerHTML));
+        }
+    }
+
+    if (needToc) {
+        document.body.className += ' hastoc';
+    }
+}
+
+generateTOC(document.getElementById('toc'));
 hljs.initHighlightingOnLoad();
 renderMathInElement(document.body);
